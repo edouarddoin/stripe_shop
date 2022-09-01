f9761f63adac081edf0341242d7b9f5550d50234
edouard doin <edouarddoin@gmail.com>
2022-09-01 18:34:54 +0200

    ajout des clefs Stripe au fichier .env

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  publishable_key: ENV['pk_test_51LY5hvLeVNdCkyHLd5NpIgd6pHbyPT9dqCEn4ETdMt4T1u2hJ5vszJ6tjtxdQcHndbyO7pUZ0one8RiUQpGCIpVZ00usgngDQM'],
-  secret_key: ENV['sk_test_51LY5hvLeVNdCkyHLNFCj4Yodi1RYADinhP6yrTAOxloa4XQKPk5OnV7sEXMWtDfrDUKx2HY6jSkXrja0bca9QhMW00FgqXiDRf'],
-  stripe_signing_secret: ENV['whsec_9116a46624382f88422561a868c15bafa3072a5e3f61f6acd8d197b0c5a9e109'] # e.g. whsec_...
+  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  secret_key: ENV['STRIPE_SECRET_KEY'],
+  stripe_signing_secret: ENV['STRIPE_SIGNING_SECRET'] # e.g. whsec_...
 }
